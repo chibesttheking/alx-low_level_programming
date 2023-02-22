c6044085c4c67115c88c274f70d495afc8154646
chibesttheking <chibesttheking@gmail.com>
2023-02-21 19:45:30 -0800

    update 5

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,22 +1,16 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code
+ * _abs - program that compute the absolute value of an integer
  *
- * Return: Always 0.
+ * @n: function parameter
+ * Return: always 0
  */
-int main(void)
-{
-    int r;
 
-    r = _abs(-1);
-    printf("%d\n", r);
-    r = _abs(0);
-    printf("%d\n", r);
-    r = _abs(1);
-    printf("%d\n", r);
-    r = _abs(-98);
-    printf("%d\n", r);
-    return (0);
+int _abs(int n)
+{
+	if (n < 0)
+		n = -(n);
+	else if (n >= 0)
+		n = n;
+	return (n);
 }
